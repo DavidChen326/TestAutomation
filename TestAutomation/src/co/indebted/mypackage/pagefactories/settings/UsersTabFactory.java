ea96969597a1935559643bf7f2e15929204afd57
David Chen <david@indebted.io>
2018-12-13 13:22:23 +1100

    Users tab refactor

--- TestAutomation/src/co/indebted/mypackage/pagefactories/settings/UsersTabFactory.java
+++ TestAutomation/src/co/indebted/mypackage/pagefactories/settings/UsersTabFactory.java
@@ -1,7 +1,11 @@
 package co.indebted.mypackage.pagefactories.settings;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -9,26 +13,106 @@ public class UsersTabFactory {
 
 	WebDriver driver;
 	
-	@FindBy(xpath = "//*[@id=\"app\"]/div/div[2]/li[4]/a")
+	@FindBy(xpath = "//*[@id=\"app\"]/div/div[2]/li[5]/a")
 	WebElement usersTab;
 	
-	@FindBy(xpath = "//*[@id=\"app\"]/div/div[3]/div[1]/div/div/fieldset/input")
-	WebElement usersSearchBox;
+	@FindBy(xpath = "//*[@id=\"app\"]/div/div[3]/div[1]/div[2]/div/button")
+	WebElement newUserButton;
 	
-	@FindBy(xpath = "//*[@id=\"app\"]/div/div[3]/div[2]/li[1]/div[1]")
-	WebElement firstUserName;
+	@FindBy(name = "first_name")
+	WebElement userFirstName;
+	
+	@FindBy(name = "last_name")
+	WebElement userLastName;
+	
+	@FindBy(name = "email")
+	WebElement userEmail;
+	
+	@FindBy(xpath = "//*[@id=\"app\"]/div/div[3]/div[3]/div/div/div[2]/div/form/div[1]/div[4]/fieldset/div[1]")
+	WebElement userRole;
+	
+	@FindBy(xpath = "//*[@id=\"app\"]/div/div[3]/div[3]/div/div/div[2]/div/form/div[1]/div[5]/fieldset/div[1]")
+	WebElement userTeam;
+	
+	@FindBy(xpath = "//*[@id=\"app\"]/div/div[3]/div[3]/div/div/div[2]/div/form/div[2]/div[2]/button")
+	WebElement saveUserButton;
+	
+	@FindBy(css = "#app > div > div:nth-child(3) > div.c0120 > div > div > div.c0126 > div > div:nth-child(1) > div > div.c0142")
+	WebElement newUserName;
+	
+	@FindBy(css = "#app > div > div:nth-child(3) > div.c0120 > div > div > div.c0126 > div > div:nth-child(2) > div > div.c0142")
+	WebElement newUserPassword;
+	
+	@FindBy(css = "#app > div > div:nth-child(3) > div.c0120 > div > div > div.c0126 > div > div:nth-child(5) > div > button")
+	WebElement doneButton;
+	
+	@FindBy(css = "#app > div > div:nth-child(3) > div.c0114 > div:nth-child(2) > div.c0117 > div.c0118.first_name")
+	WebElement firstUserFirstName;
+	
+	@FindBy(css = "#app > div > div:nth-child(3) > div.c0114 > div:nth-child(2) > div.c0119 > img")
+	WebElement firstUserEditButton;
 	
 	//get elements
 	public WebElement getUsersTab() {
 		return usersTab;
 	}
 	
-	public WebElement getUsersSearchBox() {
-		return usersSearchBox;
+	public WebElement getNewUserButton() {
+		return newUserButton;
 	}
 	
-	public WebElement getFirstUserName() {
-		return firstUserName;
+	public WebElement getUserFirstName() {
+		return userFirstName;
+	}
+	
+	public WebElement getUserLastName() {
+		return userLastName;
+	}
+	
+	public WebElement getUserEmail() {
+		return userEmail;
+	}
+	
+	public WebElement getSaveUserButton() {
+		return saveUserButton;
+	}
+	
+	public WebElement getNewUserName() {
+		return newUserName;
+	}
+	
+	public WebElement getNewUserPassword() {
+		return newUserPassword;
+	}
+	
+	public WebElement getDoneButton() {
+		return doneButton;
+	}
+	
+	public WebElement getFirstUserFirstName() {
+		return firstUserFirstName;
+	}
+	
+	public WebElement getFirstUserEditButton() {
+		return firstUserEditButton;
+	}
+	
+	public void getUserRole() {
+		//select an option from React dropdown compoment
+		Actions act = new Actions(driver);
+		act.click(userRole).build().perform();
+		List<WebElement> selectUserRole = driver.findElements(By.className("Select-input"));
+		WebElement secondRole = selectUserRole.get(1);
+		act.click(secondRole).build().perform();
+	}
+	
+	public void getUserTeam() {
+		//select an option from React dropdown compoment
+		Actions act = new Actions(driver);
+		act.click(userTeam).build().perform();
+		List<WebElement> selectUserTeam = driver.findElements(By.className("Select-input"));
+		WebElement secondTeam = selectUserTeam.get(1);
+		act.click(secondTeam).build().perform();
 	}
 	
 	//constructor
--- TestAutomation/src/co/indebted/mypackage/tests/settings/UsersTabTest.java
+++ TestAutomation/src/co/indebted/mypackage/tests/settings/UsersTabTest.java
@@ -1,11 +1,14 @@
 package co.indebted.mypackage.tests.settings;
 
+import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import co.indebted.mypackage.pagefactories.explore.ExplorePageFactory;
 import co.indebted.mypackage.pagefactories.settings.UsersTabFactory;
+import co.indebted.mypackage.tests.settings.TeamsTabTest.Team;
 import co.indebted.mypackage.utilities.LoginFactory;
+import co.indebted.mypackage.utilities.RandomStringGenerator;
 import co.indebted.mypackage.utilities.TestSetupAndTearDown;
 
 public class UsersTabTest extends TestSetupAndTearDown{
@@ -20,21 +23,60 @@ public class UsersTabTest extends TestSetupAndTearDown{
 		ExplorePageFactory explorePage = new ExplorePageFactory(driver);
 		explorePage.clickSettings();
 		
+		RandomStringGenerator randomStringGenerator = new RandomStringGenerator();
+		
 		UsersTabFactory usersTab = new UsersTabFactory(driver);
+		User user = new User(randomStringGenerator.randomString(5), randomStringGenerator.randomString(5), randomStringGenerator.randomString(5) + "@gmail.com");
 		
 		//assertions
 		//test 1
 		try {		
 			usersTab.getUsersTab().click();
+			usersTab.getNewUserButton().click();
+			usersTab.getUserFirstName().sendKeys(user.userFirstName);
+			usersTab.getUserLastName().sendKeys(user.userLastName);
+			usersTab.getUserEmail().sendKeys(user.userEmail);
+			usersTab.getUserRole();
+			usersTab.getUserTeam();
 			Thread.sleep(500);
-			usersTab.getUsersSearchBox().sendKeys("Roob");
-			
-			Assert.assertEquals(usersTab.getFirstUserName().getText(), "Clovis Roob");
+			usersTab.getSaveUserButton().click();
+			Assert.assertEquals(usersTab.getNewUserName().getText(), user.userFirstName + " " + user.userLastName);
 			Thread.sleep(1000);
 		}
 		catch(AssertionError ex){
 			System.out.println("Error: New role is not found");
 		    throw ex;
 		}
+		
+		//test 2
+		try {		
+			usersTab.getDoneButton().click();
+			usersTab.getFirstUserEditButton().click();
+			usersTab.getUserFirstName().sendKeys(user.userFirstName);
+			usersTab.getUserLastName().sendKeys(user.userLastName);
+			usersTab.getUserEmail().sendKeys(user.userEmail);
+			usersTab.getUserRole();
+			usersTab.getUserTeam();
+			Thread.sleep(500);
+			usersTab.getSaveUserButton().click();
+			Assert.assertEquals(usersTab.getFirstUserFirstName().getText(), user.userFirstName);
+			Thread.sleep(1000);
+		}
+		catch(AssertionError ex){
+			System.out.println("Error: Edit role was unsuccessful");
+		    throw ex;
+		}
 	}
+	
+	//user constructor
+		public class User{
+			public String userFirstName;
+			public String userLastName;
+			public String userEmail;
+			public User(String firstName, String lastName, String email) {
+				userFirstName = firstName;
+				userLastName = lastName;
+				userEmail = email;
+			}
+		}
 }
